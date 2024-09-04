b58f156adbfaa3cc39fb32059fdf5c966f26e364
kalinchkma <chakma.kalin10211@gmail.com>
2024-09-05 02:46:47 +0600

    server created

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+
+use axum::{routing::get, Router};
+use dotenv::dotenv;
+use std::{env, net::SocketAddr};
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let port = env::var("PORT").expect("PORT is needed to run the server");
+    let host = env::var("HOST").expect("HOST is needed to run the the server");
+
+    let host_address: SocketAddr= format!("{host}:{port}").parse().expect("Canno make an address form given host and port");
+
+    let app = Router::new().route("/", get(|| async {"Hello world"}));
+
+    // region:     -- start server
+    println!("Listening on port {}", host_address);
+    let listener = tokio::net::TcpListener::bind(host_address).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+    // endregion:  -- start server
+
+    
+}
