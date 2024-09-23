379d1fb36900e59cb1739150848ed243b5cfc906
kalinchkma <chakma.kalin10211@gmail.com>
2024-09-24 02:10:19 +0600

    integrated templating with askama

--- src/startup.rs
+++ src/startup.rs
@@ -1,8 +1,38 @@
 use std::net::SocketAddr;
 
-use axum::{routing::get, Router};
+use askama_axum::Template;
+use axum::{http::StatusCode, response::{IntoResponse, Html}, routing::get, Router};
 
 use crate::configuration::{database::DatabaseConfiguration, environtment::Environtment};
+
+// template
+#[derive(Template)]
+#[template(path = "home.html")]
+struct Home<'a> {
+    name: &'a str
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct Index<'a> {
+    name: &'a str,
+    desciption: &'a str
+}
+
+async fn home() -> impl IntoResponse {
+    let idx = Home {
+        name: "Hunter"
+    };
+    (StatusCode::OK, Html(idx.render().unwrap()))
+}
+
+async fn index() -> impl IntoResponse {
+    let idx = Index {
+        name: "Zingy",
+        desciption: "This is the first template"
+    };
+    (StatusCode::OK, Html(idx.render().unwrap()))
+}
 
 // main server stratup function
 pub async fn application() -> Result<(SocketAddr, Router), sqlx::Error> {
@@ -10,10 +40,13 @@ pub async fn application() -> Result<(SocketAddr, Router), sqlx::Error> {
     let envs = Environtment::new();
 
     // connect the database
-    let pool = DatabaseConfiguration::new(envs.db_connection_string, envs.db_max_pool).connect().await?;
+    let _pool = DatabaseConfiguration::new(envs.db_connection_string, envs.db_max_pool).connect().await?;
 
     // build appication
-    let app: Router = Router::new().route("/", get(|| async { "Hello world" }));
+    let app: Router = Router::new()
+        .route("/", get(|| async { "Hello world" }))
+        .route("/home", get(home))
+        .route("/index", get(index));
 
     // return 
     Ok((envs.address, app))
